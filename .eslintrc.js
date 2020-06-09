7b4ccd455e31611bee3a02e882f8f2ed655ad0b4
Miguel Rios <miguel@adianta.com.br>
2020-06-09 00:28:35 -0300

    linter rules

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+	env: {
+		es2020: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'plugin:@typescript-eslint/recommended',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 11,
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+		'react-hooks',
+		'@typescript-eslint',
+		'prettier',
+		'eslint-plugin-import-helpers',
+	],
+	rules: {
+		'@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+		'import/extensions': ['error', 'ignorePackages', { ts: 'never', tsx: 'never' }],
+		'import-helpers/order-imports': [
+			'error',
+			{
+				newlinesBetween: 'always',
+				groups: ['/^react/', 'module', 'parent', 'sibling', 'index'],
+				alphabetize: { order: 'asc', ignoreCase: true },
+			},
+		],
+		'import/prefer-default-export': 'off',
+		'no-unused-expressions': 'off',
+		'prettier/prettier': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'react-hooks/rules-of-hooks': 'error',
+		'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+		'react/jsx-indent': ['error', 'tab', { checkAttributes: true, indentLogicalExpressions: true }],
+		'react/jsx-indent-props': ['warn', 'tab'],
+		'react/jsx-props-no-spreading': 'off',
+		'react/prop-types': 'off',
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {},
+		},
+	},
+};
