743d97c1ed7bf5391a8073a633c664d97ec7dde9
Miguel Rios <miguel@adianta.com.br>
2020-06-15 20:06:50 -0300

    signin page styles

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logo from '../../assets/logo.png';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Title } from './styles';
+import {
+	Container,
+	Title,
+	ForgotPassword,
+	ForgotPasswordText,
+	CreateAccountButton,
+	CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
 	return (
-		<Container>
-			<Image source={logo} />
-			<Title>Faça seu logon</Title>
+		<>
+			<KeyboardAvoidingView
+				behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+				style={{ flex: 1 }}
+				enabled
+			>
+				<ScrollView contentContainerStyle={{ flex: 1 }} keyboardShouldPersistTaps="handled">
+					<Container>
+						<Image source={logo} />
+						<View>
+							<Title>Faça seu logon</Title>
+						</View>
 
-			<Input name="email" icon="mail" placeholder="E-mail" />
-			<Input name="password" icon="lock" placeholder="Senha" />
+						<Input name="email" icon="mail" placeholder="E-mail" />
+						<Input name="password" icon="lock" placeholder="Senha" />
 
-			<Button onPress={() => console.log('botao')}>Entrar</Button>
-		</Container>
+						<Button onPress={() => console.log('click botão')}>Entrar</Button>
+
+						<ForgotPassword onPress={() => console.log('click esqueci minha senha')}>
+							<ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+						</ForgotPassword>
+					</Container>
+				</ScrollView>
+			</KeyboardAvoidingView>
+
+			<CreateAccountButton onPress={() => console.log('click criar conta')}>
+				<Icon name="log-in" size={20} color="#ff9000" />
+				<CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+			</CreateAccountButton>
+		</>
 	);
 };
 
